ed081edf7e815373da7aed6c96477d0165545f2e
thabang21 <makuatbang@gmail.com>
2021-02-15 23:52:54 +0200

    adding

---
+++ readme.md
@@ -0,0 +1,3 @@
+## To run this app
+- install node modules on both server and client side
+- npm run dev to run the app 
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require("cors");
+const {v4: uuidv4} = require ('uuid');
+const app = express()
+app.use(cors());
+
+//Middeware for data on post request
+app.use(express.json({ extended: false}));
+
+// initial data
+const todos =[
+    {
+        id: "1",
+        title:"React Game!",
+        description: "Tic tac toe game created using Create Reactapp.",
+        URL: "http://heroku/myapp/game/" 
+    },
+    {
+        id: "2",
+        title:"Onlinestore",
+        description: "Online store created with HTML, CSS andJavaScript.",
+        URL: "https://git.com/myrepos/shop/index"
+    }
+]
+//gets data
+app.get("/", (req,res) => {
+    res.status(200).json(todos);
+});
+//add data
+app.post("/", (req,res)=> {
+    const newTodo= {
+        title:req.body.title,
+        id:uuidv4()
+    }
+    todos.push(newTodo)
+    res.status(201).json(todos)
+})
+
+const PORT = 8080
+app.listen(PORT, ()=> {
+    console.log(`server running on port: ${PORT}`)
+
+})
